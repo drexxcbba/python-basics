df3de9d9bd68045e26e18fae6e1dbb904ed20980
unknown <villarroel24kyle@gmail.com>
2021-06-02 19:58:50 -0400

    ft: set & dict

---
+++ setdic.py
@@ -0,0 +1,19 @@
+let = {1 , 3, 5}
+print(type(let))
+
+let.add(6)
+print(let)
+
+del let #deletes var
+
+cart = {"pizza": 2, "cookies": 3}
+
+print(dir(cart))
+
+print(cart.__class__)
+
+print(cart.keys())
+
+print(cart.values())
+
+print(len(cart))
