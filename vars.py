50e771abb084dd5c7f49e5e21246c9e1fdea2fd4
unknown <villarroel24kyle@gmail.com>
2021-06-02 17:30:56 -0400

    ft: vars

---
+++ vars.py
@@ -0,0 +1,11 @@
+name = "Rob"
+print(name)
+list = ({ "name": "Fri", "lastname": "nito"}, { "name": "Bana", "lastname": "jolito"})
+aux1 = list[0]["name"] + list[1]["lastname"]
+aux2 = list[1]["name"] + list[0]["lastname"]
+print(aux1)
+print(aux2)
+
+num_val, str_val = 1, "John"
+print(num_val)
+print(str_val)
