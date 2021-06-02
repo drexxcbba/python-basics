4a9dedb9377c1fa7fa2f8d78021c4e2b08d793a8
unknown <villarroel24kyle@gmail.com>
2021-06-02 17:16:49 -0400

    ft: datatypes

---
+++ datatype.py
@@ -0,0 +1,10 @@
+print("hello" + "papi") #concat
+#print("H" + 1) not possible
+print(type(30)) #int
+print(type("Hola")) #str
+print(type(4.1))  #float
+print(type(True)) #bool
+print(type([1, 4, 5])) #list
+print(type((1, 4, 5))) #tuple, a list that cant change
+print(type({"name":"Jan",
+            "lastname":"Guzman"})) #dict, like an object
